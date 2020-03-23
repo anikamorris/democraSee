b2c6689a46ef1aec6850f9ca2dcef34b049483ac
lauren <ljdutton2@icloud.com>
2020-03-23 15:35:44 -0700

    quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,47 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question
+from django.urls import reverse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from django.utils import timezone
 
-from django.http import HttpResponse
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'quiz/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('quiz:results', args=(question.id,)))
 
+class IndexView(generic.ListView):
+    template_name = 'quiz/quiz.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+ 
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'quiz/detail.html'
+    def get_queryset(self):
+        
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'quiz/results.html'
+
